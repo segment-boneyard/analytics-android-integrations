0bec97cc3d42d494906ca67117fc39d6844342d3
Kola Emiola <kolajubril@gmail.com>
2019-11-05 21:09:09 +0100

    DAND-10: Import amplitude-destination SDK into the analytics-android-integrations (#1)
    * DAND-10-Added: Amplitude module to analytic-android-integration mono repo project
    
    * DAND-10-Add: Bumped amplitude dependency from version 2.23.1 to 2.23.2, and android-analytics from version 4.3.1 to 4.5.0-beta.0
    
    * DAND-10-Add: Implemented amplitude and test on the sample app
    
    * DAND-10-Fix:Modified gitignore and removed DS_Store from committed files
    
    * DAND-10-Fix:Moved amplitude-module to integrations directory
    
    * DANA-10-Fix: moved .circleci and .github to root dir. Also, modified .gitignore file
    
    * DAND-10-Fix: Moved amplitude gradle folder(and contents) to the root dir
    
    * DAND-10-Fix: modified setting.gradle to point to integrations dir and applied promote.gradle plugin in project:build.gradle

---
+++ sample/src/main/java/com/segment/analytics/android/integrations/SampleApp.java
@@ -0,0 +1,43 @@
+package com.segment.analytics.android.integrations;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.segment.analytics.Analytics;
+import com.segment.analytics.android.integrations.amplitude.AmplitudeIntegration;
+
+public class SampleApp extends Application {
+
+    // https://segment.com/segment-engineering/sources/android-test/settings/keys
+    private static final String ANALYTICS_WRITE_KEY = "zHGrqk0zY6eV7V6E74VsGZHykfXisd91";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize a new instance of the Analytics client.
+        Analytics.Builder builder =
+                new Analytics.Builder(this, ANALYTICS_WRITE_KEY)
+                        .use(AmplitudeIntegration.FACTORY)
+                        .trackApplicationLifecycleEvents()
+                        .trackAttributionInformation()
+                        .recordScreenViews();
+
+        // Set the initialized instance as a globally accessible instance.
+        Analytics.setSingletonInstance(builder.build());
+
+        // Now anytime you call Analytics.with, the custom instance will be returned.
+        Analytics analytics = Analytics.with(this);
+
+        // If you need to know when integrations have been initialized, use the onIntegrationReady
+        // listener.
+        analytics.onIntegrationReady(
+                "Segment.io",
+                new Analytics.Callback() {
+                    @Override
+                    public void onReady(Object instance) {
+                        Log.d("Segment Sample", "Segment integration ready.");
+                    }
+                });
+    }
+}
