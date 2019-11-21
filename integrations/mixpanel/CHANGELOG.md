e0ababefbefe94498aa8986fe0134ed35da9d13e
Brennan Gamwell <brennan@segment.com>
2019-11-20 16:57:52 -0800

    Migrate Mixpanel SDK (#5)
    * DAND-29-Add: Import mixpanel-module into the project, moved the module into integrations directory and set it up in project:setting.sgradle
    
    * DAND-29-Fix: Removed gradle folder(wrapper/, attach-jar,promote.gradle, sign.gradle and upload) and .circleci in the mixpanel module
    
    * DAND-29-Fix: Remove buildscript tag in mixpanel:build.gradle, set it up in the root:build.gradle and bump segement-analytics to 4.5.0-beta.0, mixpanel to 5.6.5 in mixpanel:build.gradle and gradle plugin to 3.5.2 in project:build.gradle
    
    * DAND-29-Update: Bump all test(robolectric,mockito-core, powermock) dependencies to latest. add skyscreamer:jsonassert in the mixpanel:build.gradle and modify mixpanelTest for the dependencies updates. all 31 test passed, ./gradlew test => build successful
    
    * DAND-29-Fix: Update Changelog.md and gradle.properties, Remove .buildscript(deploy_snapshot.sh and android-sdk-license)

---
+++ integrations/mixpanel/CHANGELOG.md
@@ -0,0 +1,89 @@
+Version 2.2.0 (15th November, 2019)
+==================================
+*(Supports Mixpanel 5.6.5)*
+
+  * Bumps Mixpanel version to 5.6.5
+  * Mixpanel version 5.6.5 fixes and updates [here](https://github.com/mixpanel/mixpanel-android/releases/tag/v5.6.5)
+
+Version 2.1.1 (12th September, 2019)
+==================================
+*(Supports Mixpanel 5.6.3)*
+
+  * Bumps Mixpanel version to 5.6.3
+  * Mixpanel version 5.6.3 fixes and updates [here](https://github.com/mixpanel/mixpanel-android/releases/tag/v5.6.3)
+
+Version 2.1.0 (5th June, 2019)
+==================================
+*(Supports Mixpanel 5.6.2)*
+
+  * Bumps Mixpanel version to 5.6.2
+  * Mixpanel version 5.6.2 fixes and updates [here](https://github.com/mixpanel/mixpanel-android/releases/tag/v5.6.2)
+  * Bumps Analytics SDK version to 4.3.1
+
+Version 2.0.0 (7th November, 2017)
+==================================
+*(Supports Mixpanel 5.2.1)*
+
+  * Bumps Mixpanel version to 5.2.1
+  * Mixpanel version 5.2.1 fixes and updates [here](https://github.com/mixpanel/mixpanel-android/releases/tag/v5.2.1)
+
+Version 1.0.8 (2nd February, 2017)
+===================================
+*(Supports Mixpanel 4.9.8)*
+
+  * Fix: Don't mutate properties object in payload.
+
+Version 1.0.7 (2nd February, 2017)
+===================================
+*(Supports Mixpanel 4.9.2)*
+
+  * [Bug](https://github.com/segment-integrations/analytics-android-integration-mixpanel/pull/14): While `setAllTraitsByDefault` is false, this pulls in `peopleProperties` and/or `superProperties` when configured in the integration panel.
+  * Refactor tests.
+
+
+Version 1.0.6 (5th December, 2016)
+===================================
+*(Supports Mixpanel 4.9.2)*
+
+  * [Improvement](https://github.com/segment-integrations/analytics-android-integration-mixpanel/pull/12): Explicitly send distinct ID as previous ID in alias call.
+
+Version 1.0.5 (5th December, 2016)
+===================================
+*(Supports Mixpanel 4.9.2)*
+
+  * Bumps Mixpanel version to 4.9.2
+  * Mixpanel version 4.9.2 fixes and updates [here](https://github.com/mixpanel/mixpanel-android/releases/tag/v4.9.2)
+
+
+Version 1.0.4 (6th July, 2016)
+===================================
+*(Supports Mixpanel 4.9.0)*
+
+  * Mixpanel version 4.9.0 fixes:
+  * Activity life cycle callbacks in old Android APIs.
+  * OutOfMemoryError for in-app and ab test.
+  * NullPointerException when accessing people profiles after resetting a Mixpanel instance.
+
+Version 1.0.3 (12th May, 2016)
+===================================
+*(Supports analytics-android 4.0.+ and Mixpanel 4.7.+)*
+
+  * Add support for Segment's consolidatedPageCalls setting to send all Screen calls to Mixpanel as `Loaded a Screen`
+
+Version 1.0.2 (12th May, 2016)
+===================================
+*(Supports analytics-android 4.0.+ and Mixpanel 4.7.+)*
+
+  * Do not call Mixpanel's identify if no userId has been set.
+
+Version 1.0.1 (2nd May, 2016)
+===================================
+*(Supports analytics-android 4.0.+ and Mixpanel 4.7.+)*
+
+  * Fix mapping of firstName and name.
+
+Version 1.0.0 (27th November, 2015)
+===================================
+*(Supports analytics-android 4.0.+ and Mixpanel 4.7.+)*
+
+  * Initial Release.
---
+++ integrations/mixpanel/src/test/java/com/segment/analytics/android/integrations/mixpanel/MixpanelTest.java
@@ -0,0 +1,475 @@
+package com.segment.analytics.android.integrations.mixpanel;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Build;
+import android.os.Bundle;
+import com.google.common.collect.ImmutableMap;
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+import com.segment.analytics.Analytics;
+import com.segment.analytics.Properties;
+import com.segment.analytics.Traits;
+import com.segment.analytics.ValueMap;
+import com.segment.analytics.integrations.IdentifyPayload;
+import com.segment.analytics.integrations.Logger;
+import com.segment.analytics.test.AliasPayloadBuilder;
+import com.segment.analytics.test.IdentifyPayloadBuilder;
+import com.segment.analytics.test.ScreenPayloadBuilder;
+import com.segment.analytics.test.TrackPayloadBuilder;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import static com.segment.analytics.Utils.createTraits;
+import static com.segment.analytics.android.integrations.mixpanel.MixpanelIntegration.filter;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyNoMoreInteractions;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = Build.VERSION_CODES.P, manifest = Config.NONE)
+@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*", "org.json.*" })
+@PrepareForTest(MixpanelAPI.class) public class MixpanelTest {
+
+  @Rule public PowerMockRule rule = new PowerMockRule();
+  @Mock MixpanelAPI mixpanel;
+  @Mock Application context;
+  @Mock MixpanelAPI.People mixpanelPeople;
+  @Mock Analytics analytics;
+
+  MixpanelIntegration integration;
+
+  @Before public void setUp() {
+    initMocks(this);
+    mockStatic(MixpanelAPI.class);
+    Logger logger = Logger.with(Analytics.LogLevel.DEBUG);
+    when(MixpanelAPI.getInstance(context, "foo")).thenReturn(mixpanel);
+    when(mixpanel.getPeople()).thenReturn(mixpanelPeople);
+    when(analytics.logger("Mixpanel")).thenReturn(logger);
+    when(analytics.getApplication()).thenReturn(context);
+
+    integration =
+        new MixpanelIntegrationBuilder().setMixpanel(mixpanel).createMixpanelIntegration();
+  }
+
+  @Test public void factory() {
+    ValueMap settings = new ValueMap().putValue("token", "foo")
+        .putValue("trackAllPages", true)
+        .putValue("trackCategorizedPages", false)
+        .putValue("trackNamedPages", true)
+        .putValue("setAllTraitsByDefault", true);
+
+    MixpanelIntegration integration =
+        (MixpanelIntegration) MixpanelIntegration.FACTORY.create(settings, analytics);
+
+    MixpanelAPI.getInstance(context, "foo");
+    verify(mixpanel, never()).getPeople();
+
+    assertThat(integration.token).isEqualTo("foo");
+    assertThat(integration.trackAllPages).isTrue();
+    assertThat(integration.trackCategorizedPages).isFalse();
+    assertThat(integration.trackNamedPages).isTrue();
+    assertThat(integration.increments).isNotNull().isEmpty();
+    assertThat(integration.setAllTraitsByDefault).isTrue();
+  }
+
+  @Test public void initializeWithIncrementsAndPeople() throws IllegalStateException {
+    ValueMap settings = new ValueMap().putValue("token", "foo")
+        .putValue("people", true)
+        .putValue("trackAllPages", true)
+        .putValue("trackCategorizedPages", false)
+        .putValue("trackNamedPages", true)
+        .putValue("increments", Arrays.asList("baz", "qaz", "qux"));
+
+    MixpanelIntegration integration =
+        (MixpanelIntegration) MixpanelIntegration.FACTORY.create(settings, analytics);
+
+    MixpanelAPI.getInstance(context, "foo");
+    verify(mixpanel).getPeople();
+    assertThat(integration.token).isEqualTo("foo");
+    assertThat(integration.trackAllPages).isTrue();
+    assertThat(integration.trackCategorizedPages).isFalse();
+    assertThat(integration.trackNamedPages).isTrue();
+    verify(mixpanel).getPeople();
+    // Don't use containsExactly since the ordering differs between JDK versions.
+    assertThat(integration.increments).hasSize(3).contains("qux", "baz", "qaz");
+  }
+
+  @Test public void activityCreate() {
+    Activity activity = mock(Activity.class);
+    Bundle bundle = mock(Bundle.class);
+    integration.onActivityCreated(activity, bundle);
+    verifyNoMoreInteractions(mixpanel);
+  }
+
+  @Test public void activityStart() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityStarted(activity);
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void activityResume() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityResumed(activity);
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void activityPause() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityPaused(activity);
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void activityStop() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityStopped(activity);
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void activitySaveInstance() {
+    Activity activity = mock(Activity.class);
+    Bundle bundle = mock(Bundle.class);
+    integration.onActivitySaveInstanceState(activity, bundle);
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void activityDestroy() {
+    Activity activity = mock(Activity.class);
+    integration.onActivityDestroyed(activity);
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void screen() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .setConsolidatedPageCalls(false)
+        .createMixpanelIntegration();
+
+    integration.screen(new ScreenPayloadBuilder().name("foo").build());
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void screenAllPages() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setConsolidatedPageCalls(false)
+        .setTrackAllPages(true)
+        .createMixpanelIntegration();
+
+    integration.screen(new ScreenPayloadBuilder().name("foo").build());
+    verify(mixpanel).track(eq("Viewed foo Screen"), jsonEq(new JSONObject()));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void screenConsolidatedPages() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .setTrackNamedPages(true)
+        .createMixpanelIntegration();
+
+    integration.screen(new ScreenPayloadBuilder().name("foo").build());
+    Properties properties = new Properties();
+    properties.put("name", "foo");
+    verify(mixpanel).track(eq("Loaded a Screen"), jsonEq(properties.toJsonObject()));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void screenNamedPages() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .setConsolidatedPageCalls(false)
+        .setTrackNamedPages(true)
+        .createMixpanelIntegration();
+
+    integration.screen(new ScreenPayloadBuilder().name("foo").build());
+    verify(mixpanel).track(eq("Viewed foo Screen"), jsonEq(new JSONObject()));
+    verifyNoMoreMixpanelInteractions();
+
+    integration.screen(new ScreenPayloadBuilder().category("foo").build());
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void screenCategorizedPages() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .setConsolidatedPageCalls(false)
+        .setTrackCategorizedPages(true)
+        .createMixpanelIntegration();
+
+    integration.screen(new ScreenPayloadBuilder().category("foo").build());
+    verify(mixpanel).track(eq("Viewed foo Screen"), jsonEq(new JSONObject()));
+    verifyNoMoreMixpanelInteractions();
+
+    integration.screen(new ScreenPayloadBuilder().name("foo").build());
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void track() {
+    integration.track(new TrackPayloadBuilder().event("foo").build());
+    verify(mixpanel).track(eq("foo"), jsonEq(new JSONObject()));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void trackIncrement() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .setConsolidatedPageCalls(false)
+        .setTrackAllPages(true)
+        .setTrackCategorizedPages(true)
+        .setTrackNamedPages(true)
+        .setIncrements(Collections.singleton("baz"))
+        .createMixpanelIntegration();
+
+    integration.track(new TrackPayloadBuilder().event("baz").build());
+
+    verify(mixpanel).track(eq("baz"), jsonEq(new JSONObject()));
+    verify(mixpanelPeople).increment("baz", 1);
+    verify(mixpanelPeople).set(eq("Last baz"), any());
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void trackIncrementWithoutPeople() {
+    // Disabling people should do a regular track call
+    integration.track(new TrackPayloadBuilder().event("baz").build());
+    verify(mixpanel).track(eq("baz"), jsonEq(new JSONObject()));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void alias() {
+    integration.alias(new AliasPayloadBuilder().traits(createTraits("foo")).newId("bar") //
+        .build());
+    verify(mixpanel).alias("bar", "foo");
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void aliasWithoutAnonymousId() {
+    String mpDistinctId = "mpDistinctId";
+    when(mixpanel.getDistinctId()).thenReturn(mpDistinctId);
+    integration.alias(new AliasPayloadBuilder().traits(new Traits() //
+        .putValue("anonymousId", "qaz")).newId("qux").build());
+    verify(mixpanel).alias("qux", mpDistinctId);
+    verify(mixpanel).getDistinctId();
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void identify() {
+    assertThat(integration.mixpanelPeople).isNull();
+    assertThat(integration.isPeopleEnabled).isFalse();
+
+    integration.identify(new IdentifyPayload.Builder()
+        .userId("prateek")
+        .traits(new Traits().putAge(25))
+        .build());
+    verify(mixpanel).identify("prateek");
+    verify(mixpanel).registerSuperProperties(jsonEq(new JSONObject(ImmutableMap.of("age", 25))));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void identifyWithoutUserId() {
+    integration.identify(new IdentifyPayload.Builder()
+        .anonymousId("anonymousId")
+        .traits(new Traits().putAge(25))
+        .build());
+    verify(mixpanel, never()).identify(anyString());
+    verify(mixpanel).registerSuperProperties(jsonEq(new JSONObject(ImmutableMap.of("age", 25))));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void identifyWithPeople() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .createMixpanelIntegration();
+
+    integration.identify(new IdentifyPayload.Builder()
+        .traits(new Traits().putAge(25))
+        .userId("prateek")
+        .build());
+
+    verify(mixpanel).identify("prateek");
+    verify(mixpanel).registerSuperProperties(jsonEq(new JSONObject(ImmutableMap.of("age", 25))));
+    verify(mixpanelPeople).identify("prateek");
+    verify(mixpanelPeople).set(jsonEq(new JSONObject(ImmutableMap.of("age", 25))));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void identifyWithSuperProperties() throws JSONException {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .createMixpanelIntegration();
+
+    Traits traits = new Traits()
+        .putEmail("friends@segment.com")
+        .putPhone("1-844-611-0621")
+        .putCreatedAt("15th Feb, 2015")
+        .putUsername("segmentio");
+    JSONObject expected = new JSONObject();
+    expected.put("$email", traits.email());
+    expected.put("$phone", traits.phone());
+    expected.put("$first_name", traits.firstName());
+    expected.put("$last_name", traits.lastName());
+    expected.put("$name", traits.name());
+    expected.put("$username", traits.username());
+    expected.put("$created", traits.createdAt());
+
+    integration.identify(new IdentifyPayload.Builder()
+        .userId("prateek")
+        .traits(traits)
+        .build());
+    verify(mixpanel).identify("prateek");
+    verify(mixpanel).registerSuperProperties(jsonEq(expected));
+    verify(mixpanelPeople).identify("prateek");
+    verify(mixpanelPeople).set(jsonEq(expected));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void identifyWithSuperPropertiesValues() throws JSONException {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .setSuperProperties(Collections.singleton("parasite"))
+        .setSetAllTraitsByDefault(false)
+        .createMixpanelIntegration();
+    Traits traits = createTraits("foo").putEmail("Raptor@segment.com")
+        .putValue("parasite", "Photography Raptor");
+    JSONObject expected = new JSONObject();
+    expected.put("parasite", "Photography Raptor");
+
+    integration.identify(new IdentifyPayloadBuilder(
+
+    ).traits(traits).build());
+    verify(mixpanel).identify("foo");
+    verify(mixpanel).registerSuperProperties(jsonEq(expected));
+    verify(mixpanelPeople).identify("foo");
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void identifyWithPeopleProperties() throws JSONException {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .setPeopleProperties(Collections.singleton("parasite"))
+        .setSetAllTraitsByDefault(false)
+        .createMixpanelIntegration();
+
+    Traits traits = createTraits("foo").putEmail("Pencilvester@segment.com")
+        .putValue("parasite", "Pencilvester");
+    JSONObject expected = new JSONObject();
+    expected.put("parasite", "Pencilvester");
+
+    integration.identify(new IdentifyPayloadBuilder().traits(traits).build());
+    verify(mixpanel).identify("foo");
+    verify(mixpanelPeople).set(jsonEq(expected));
+    verify(mixpanelPeople).identify("foo");
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void testFilter() {
+    Map<String, String> map = Collections.singletonMap("foo", "bar");
+    assertThat(filter(map, Collections.<String>emptySet())).isEqualTo(Collections.emptyMap());
+    assertThat(filter(map, Collections.singletonList("bar"))).isEqualTo(Collections.emptyMap());
+    assertThat(filter(map, Collections.singletonList("foo"))).isEqualTo(
+        Collections.singletonMap("foo", "bar"));
+  }
+
+  @Test public void event() {
+    Properties properties = new Properties().putRevenue(20);
+    integration.event("foo", properties);
+    verify(mixpanel).track(eq("foo"), jsonEq(properties.toJsonObject()));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void eventWithPeople() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .createMixpanelIntegration();
+    Properties properties = new Properties();
+    integration.event("foo", properties);
+    verify(mixpanel).track(eq("foo"), jsonEq(properties.toJsonObject()));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void eventWithPeopleAndRevenue() {
+    integration = new MixpanelIntegrationBuilder().setMixpanel(mixpanel)
+        .setMixpanelPeople(mixpanelPeople)
+        .setIsPeopleEnabled(true)
+        .createMixpanelIntegration();
+    Properties properties = new Properties().putRevenue(20);
+    integration.event("foo", properties);
+    verify(mixpanel).track(eq("foo"), jsonEq(properties.toJsonObject()));
+    verify(mixpanelPeople).trackCharge(eq(20.0), jsonEq(properties.toJsonObject()));
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void flush() {
+    integration.flush();
+    verify(mixpanel).flush();
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  @Test public void reset() {
+    integration.reset();
+    verify(mixpanel).reset();
+    verifyNoMoreMixpanelInteractions();
+  }
+
+  private void verifyNoMoreMixpanelInteractions() {
+    verifyNoMoreInteractions(MixpanelAPI.class);
+    verifyNoMoreInteractions(mixpanel);
+    verifyNoMoreInteractions(mixpanelPeople);
+  }
+
+  private JSONObject jsonEq(JSONObject expected) {
+    return argThat(new JSONMatcher(expected));
+  }
+
+  class JSONMatcher implements ArgumentMatcher<JSONObject> {
+    JSONObject expected;
+
+    JSONMatcher(JSONObject expected) {
+      this.expected = expected;
+    }
+
+    @Override
+    public boolean matches(JSONObject argument) {
+      try {
+        JSONAssert.assertEquals(expected, argument, JSONCompareMode.STRICT);
+        return true;
+      } catch (JSONException e) {
+        return false;
+      }
+    }
+  }
+
+}
