a873dbcfad3188c41e33a81f7bbcb85cf7851d92
Kola Emiola <kolaemiola@Andelas-MacBook-Pro.local>
2019-12-09 16:28:10 +0100

    DAND-21-Fix: Added tracking lifecycle event with checkbox

--- sample/src/main/java/com/segment/analytics/android/integrations/SampleApp.java
+++ sample/src/main/java/com/segment/analytics/android/integrations/SampleApp.java
@@ -1,10 +1,10 @@
 package com.segment.analytics.android.integrations;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 import android.util.Log;
-import com.segment.analytics.Analytics;
-import com.segment.analytics.android.integrations.amplitude.AmplitudeIntegration;
 
+import com.segment.analytics.Analytics;
 public class SampleApp extends Application {
 
   // https://segment.com/segment-engineering/sources/android-test/settings/keys
@@ -13,30 +13,22 @@ public class SampleApp extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
-
+    SharedPreferences prefs = this.getSharedPreferences(getString(R.string.PACKAGE_NAME), MODE_PRIVATE);
+    boolean trackLifeCycle = prefs.getBoolean("trackLifeCycle",false);
+    Log.d("trackLifeCycle",String.valueOf(trackLifeCycle));
     // Initialize a new instance of the Analytics client.
-    Analytics.Builder builder =
+      Analytics.Builder builder =
         new Analytics.Builder(this, ANALYTICS_WRITE_KEY)
-            .use(AmplitudeIntegration.FACTORY)
             .trackAttributionInformation()
             .recordScreenViews();
+
+   if(trackLifeCycle){
+     builder.trackApplicationLifecycleEvents();
+   }
 
     // Set the initialized instance as a globally accessible instance.
     Analytics.setSingletonInstance(builder.build());
 
-    // Now anytime you call Analytics.with, the custom instance will be returned.
-    Analytics analytics = Analytics.with(this);
-
-    // If you need to know when integrations have been initialized, use the onIntegrationReady
-    // listener.
-    analytics.onIntegrationReady(
-        "Segment.io",
-        new Analytics.Callback() {
-          @Override
-          public void onReady(Object instance) {
-            Log.d("Segment Sample", "Segment integration ready.");
-          }
-        });
-
   }
+
 }
--- sample/src/main/java/com/segment/analytics/android/integrations/SpecListFragment.java
+++ sample/src/main/java/com/segment/analytics/android/integrations/SpecListFragment.java
@@ -2,19 +2,25 @@ package com.segment.analytics.android.integrations;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.segment.analytics.Analytics;
 import com.segment.analytics.android.integrations.databinding.FragmentSpecListBinding;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 /**
@@ -23,8 +29,6 @@ import java.util.List;
 public class SpecListFragment extends Fragment {
 
     private FragmentSpecListBinding binding;
-    String specText;
-
     public SpecListFragment() {
         // Required empty public constructor
     }
@@ -35,29 +39,47 @@ public class SpecListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_spec_list, container, false);
-        setupUI();
+        setupSpecsUI();
+        trackAppLifeCycle();
+        binding.gotoDocs.setOnClickListener(v -> gotoDocumentation());
         return binding.getRoot();
     }
 
-    private void setupUI(){
+    private void setupSpecsUI(){
         binding.specList.hasFixedSize();
-        SpecClickListener specClickListener = string -> {};
+        SpecClickListener specClickListener = string -> {
+            SpecDetailFragment specDetailFragment = SpecDetailFragment.getSpec(string);
+            getFragmentManager().beginTransaction().replace(R.id.parentContainer, specDetailFragment)
+                    .addToBackStack("").commit();
+        };
         String [] specArray = getResources().getStringArray(R.array.spec_list);
         List<String> speclist = Arrays.asList(specArray);
         SpecListAdapter specListAdapter = new SpecListAdapter(speclist,specClickListener);
         binding.specList.setAdapter(specListAdapter);
         specListAdapter.setSpecList(speclist);
+
     }
 
     private void trackAppLifeCycle(){
-        if(binding.trackAppLifecycle.isChecked()){
-            //todo
-        }
+        binding.trackAppLifecycle.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            SharedPreferences.Editor editor = getActivity()
+                    .getSharedPreferences(getString(R.string.PACKAGE_NAME), MODE_PRIVATE).edit();
+            if(isChecked){
+                editor.putBoolean("trackLifeCycle",true);
+                editor.apply();
+            }else {
+                editor.putBoolean("trackLifeCycle",false);
+                editor.apply();
+            }
+        });
+        SharedPreferences prefs = getContext().getSharedPreferences(getString(R.string.PACKAGE_NAME), MODE_PRIVATE);
+        binding.trackAppLifecycle.setChecked(prefs.getBoolean("trackLifeCycle",false));
+
     }
 
     private void gotoDocumentation(){
         Intent browserIntent = new Intent(Intent.ACTION_VIEW);
-        browserIntent.setData(Uri.parse(""));
+        browserIntent.setData(Uri.parse("https://segment.com/docs/connections/spec/"));
         startActivity(browserIntent);
     }
 }
